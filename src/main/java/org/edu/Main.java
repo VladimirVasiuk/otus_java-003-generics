b55fed7e0b24fdc3ce4b8b0259c63bbb238be169
ydvorzhetskiy <dvorzhetskij@mail.ru>
2018-09-29 09:13:39 +0600

    Final

---
+++ src/main/java/org/edu/Main.java
@@ -0,0 +1,21 @@
+package org.edu;
+
+import java.util.Objects;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // TODO: создайте класс Pair, чтобы все следующие строки компилировались и выполнянлись корректно
+
+        //Pair<String, Integer> pair = new Pair<String, Integer>("Hello", 12);
+
+        //assert Objects.equals("Hello", pair.getLeft());
+
+        //assert Objects.equals(12, pair.getRight());
+
+        //assert !pair.equals(new Pair<String, Long>("Hello", 12L));
+
+        //pair = Pair.of("World", "!");
+    }
+}
